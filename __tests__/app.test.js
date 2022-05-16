7f15a4a6a47157332596e770a9afb3fbb62620a6
philipwalton123 <philipwalton123@googlemail.com>
2022-05-16 12:27:55 +0100

    pass initial test for GET/api/topics (type = array)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,23 @@
 const app = require ('../app')
 const supertest = require('supertest')
+const seed = require('../db/seeds/seed')
+const db = require('../db/connection')
+const testData = require('../db/data/test-data/index')
+
+beforeEach(() => {
+return seed(testData)
+})
+
+afterAll(() => {
+    db.end()
+})
 
 describe('GET /api/topics', () => {
     it('should return an array of all topics', () => {
         return supertest(app).get('/api/topics')
         .expect(200)
         .then(result => {
-            expect(Array.isArray(result.body)).toBe(true)
+            expect(Array.isArray(result.body.topics)).toBe(true)
         })
     });
 });
