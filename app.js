55542e69f86d34757c3a3fb8b3f1c63cf8c88115
philipwalton123 <philipwalton123@googlemail.com>
2022-05-16 16:18:46 +0100

    add endpoint and functionality for GET /api/articles/article_id

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
-const { getTopics } = require('./controllers/topics.controllers')
+const { getTopics, getArticle } = require('./controllers/topics.controllers')
 
 const app = express()
 
+//GET all topics
 app.get('/api/topics', getTopics)
 
+//GET article by article_id number
+app.get('/api/articles/:article_id', getArticle)
+
 app.use('/*', (req, res, next) => {
-    console.log("caught the error in /*")
     res.status(404).send({msg: "not found"})
+    next()
+})
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+        res.status(400).send({msg: `not a valid request`})
+    } else {
+        next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status).send({msg: err.msg})
 })
 
 module.exports = app
