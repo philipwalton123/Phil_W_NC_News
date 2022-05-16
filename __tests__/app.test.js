dd63869592b6a1e43f9ee8261efc687163c35462
philipwalton123 <philipwalton123@googlemail.com>
2022-05-16 12:42:26 +0100

    add ability to catch typo error and respond 'not found'

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,11 +13,19 @@ afterAll(() => {
 })
 
 describe('GET /api/topics', () => {
-    it('should return an array of all topics', () => {
+    it('200: should return an array of all topics', () => {
         return supertest(app).get('/api/topics')
         .expect(200)
-        .then(result => {
-            expect(Array.isArray(result.body.topics)).toBe(true)
+        .then(response => {
+            expect(Array.isArray(response.body.topics)).toBe(true)
+            expect(response.body.topics).toHaveLength(3)
+        })
+    });
+    it('404: typo in url should respond with not found', () => {
+        return supertest(app).get('/api/topis')
+        .expect(404)
+        .then(response => {
+            expect(response.body.msg).toBe("not found")
         })
     });
 });
--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const app = express()
 
 app.get('/api/topics', getTopics)
 
+app.use('/*', (req, res, next) => {
+    console.log("caught the error in /*")
+    res.status(404).send({msg: "not found"})
+})
+
 module.exports = app
