157502c15dbc008d4066b59f7bdfa30982ea7106
philipwalton123 <philipwalton123@googlemail.com>
2022-05-17 13:41:10 +0100

    remove commented-out older version of logic

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,28 +1,6 @@
 const db = require('../db/connection')
 
 exports.getThisArticle = (id) => {
-    // const article = db.query('SELECT * FROM articles WHERE article_id = $1', [id])
-    // .then(result => {
-    //     if(result.rows.length == 0){
-    //         return Promise.reject({status: 404, msg: `no such article with id ${id}`})
-    //     } else {
-    //         return result.rows[0]
-    //     }
-    // })
-
-    // const count = db.query('SELECT COUNT (article_id) FROM comments WHERE article_id = $1', [id])
-    // .then(result => {
-    //     return result.rows[0].count
-    // })
-
-    // return Promise.all([article, count])
-    // .then(([article, count]) => {
-    //     article.comment_count = parseInt(count, 10)
-    //     return article
-    // })
-
-    //REFACTOR INTO A SINGLE QUERY
-
     return db.query('SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles. created_at, articles.votes, CAST(COUNT (*) AS INT) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE comments.article_id = $1 GROUP BY articles.article_id', [id])
     .then(result => {
         console.log(result.rows, '<<<model')
