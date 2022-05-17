45907b16bd21bcb3d2401e0964bfabdd078399da
philipwalton123 <philipwalton123@googlemail.com>
2022-05-17 15:58:34 +0100

    create model utils with valueExists function returning boolean

---
+++ models/modelUtils.js
@@ -0,0 +1,7 @@
+exports.valueExists = (table, column, value) => {
+    const queryStr = format('SELECT * FROM %I WHERE %I = $1;', table, column)
+    return db.query(queryStr, [value])
+    .then(result => {
+        return result.rows.length >= 1
+    })
+}
