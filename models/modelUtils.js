f0c86cf55dee4eb95d44810cc31c05dbe66fd3bb
philipwalton123 <philipwalton123@googlemail.com>
2022-05-17 16:34:37 +0100

    fix error where promise was not being returned

--- models/modelUtils.js
+++ models/modelUtils.js
@@ -1,3 +1,6 @@
+const format = require('pg-format')
+const db = require('../db/connection')
+
 exports.valueExists = (table, column, value) => {
     const queryStr = format('SELECT * FROM %I WHERE %I = $1;', table, column)
     return db.query(queryStr, [value])
