ca97e1b006c86e0a71d20ee0b395eb31d8599bd4
philipwalton123 <philipwalton123@googlemail.com>
2022-05-18 19:45:39 +0100

    remove need for pg-format

--- models/modelUtils.js
+++ models/modelUtils.js
@@ -1,9 +1,14 @@
-const format = require('pg-format')
 const db = require('../db/connection')
 
 exports.valueExists = (table, column, value) => {
-    const queryStr = format('SELECT * FROM %I WHERE %I = $1;', table, column)
-    return db.query(queryStr, [value])
+    const tableGreenList = ["users", "topics", "articles", "comments"]
+    const columnsGreenLists = ["title", "topic", "author", "body", "created_at", "votes", "article_id", "description", "slug", "username", "name", "avatar_url"]
+    
+    if(!tableGreenList.includes(table) || !columnsGreenLists.includes(column)) {
+        return false
+    }
+    
+    return db.query(`SELECT * FROM ${table} WHERE ${column} = $1`, [value])
     .then(result => {
         return result.rows.length >= 1
     })
