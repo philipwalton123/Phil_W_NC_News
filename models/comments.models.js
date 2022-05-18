160f92cafb769f15422a1f6c5b11f5de6357e28f
philipwalton123 <philipwalton123@googlemail.com>
2022-05-18 15:06:25 +0100

    creat comments controllers and models, with delete comment functions

---
+++ models/comments.models.js
@@ -0,0 +1,14 @@
+const db = require('../db/connection')
+
+exports.removeThisComment = (id) => {
+    console.log(id)
+    return db.query('DELETE FROM comments WHERE comment_id = $1 RETURNING *', [id])
+    .then(result => {
+        if(result.rows.length) {
+            return result.rows[0]
+        } else {
+            return Promise.reject({status:404, msg: 'comment not found'})
+        }
+        
+    })
+}
