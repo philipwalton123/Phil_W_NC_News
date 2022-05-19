2e565d07adb883e3f50a1d28dac12993e3cf5ac8
philipwalton123 <philipwalton123@googlemail.com>
2022-05-19 14:01:03 +0100

    introduce articles-router paths to api-router

--- api-router.js
+++ api-router.js
@@ -1,7 +1,10 @@
 const apiRouter = require('express').Router()
+const articleRouter = require('./articles-router')
 
 apiRouter.get('/', (req, res) => {
     res.status(200).send("All is well from apiRouter")
 })
+
+apiRouter.get('/articles', articleRouter)
 
 module.exports = apiRouter
